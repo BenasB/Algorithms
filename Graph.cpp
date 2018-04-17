731f58b89f5cc1dfc5cff806a639ebe9d26b269d
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2018-04-17 19:05:24 +0300

    C++ basic graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,54 +1,39 @@
 #include <iostream>
 #include <map>
-#include <set>
-
-using namespace std;
+#include <vector>
 
 class Node
 {
-    public:
-        Node(int id);
-        set<Node> adjacent;
-    protected:
-        int id;
+public:
+    std::vector<Node*> adjacent;
+    Node(int id): ID(id) {}
+private:
+    int ID;
 };
-
-Node::Node(int id)
-{
-    this->id = id;
-}
 
 class Graph
 {
-    public:
-        Graph(bool bidirectional);
-        void addEdge(int start, int destination);
-    private:
-        bool bidirectional;
-        map<int, Node> nodeLookup;
-        Node getNode(int id);
+public:
+    void addEdge(int start, int destination)
+    {
+        Node* s = getNode(start);
+        Node* d = getNode(destination);
+        s->adjacent.push_back(d);
+    }
+    Graph(bool bidirect):bidirectional(bidirect){}
+private:
+    typedef std::map<int, Node*> inmap;
+    inmap nodeLookup;
+    bool bidirectional;
+    Node* getNode(int id)
+    {
+        inmap::iterator it = nodeLookup.find(id);
+        if (it != nodeLookup.end())
+            return it->second;
+        else
+        {
+            Node newNode(id);
+            return nodeLookup[id] = &newNode;
+        }
+    }
 };
-
-Graph::Graph(bool bidirectional)
-{
-    this->bidirectional = bidirectional;
-}
-
-void Graph::getNode(int id)
-{
-
-}
-
-void Graph::addEdge(int start, int destination)
-{
-    Node s = getNode(start);
-    Node d = getNode(destination);
-    s.adjacent.insert(d);
-    if (bidirectional)
-        d.adjacent.insert(s);
-}
-
-int main()
-{
-
-}
